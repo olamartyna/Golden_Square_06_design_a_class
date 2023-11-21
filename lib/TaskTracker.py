1d6b4ce9c268ef3d9b0ace04086fcb5ad500bd2b
Tejaswini Dodwade <tejaswinidodwade@MASTUDENT-Tejaswini-Dodwade-FVFLF2P61WFY.local>
2023-11-21 12:22:00 +0000

    Adding and removing multiple tasks at onece

--- lib/TaskTracker.py
+++ lib/TaskTracker.py
@@ -2,16 +2,25 @@
 # So that I can keep track of my tasks
 # I want a program that I can add todo tasks to and see a list of them.
 
+# As a user
+# So that I can focus on tasks to complete
+# I want to mark tasks as complete and have them disappear from the list.
 class TaskTracker:
     def __init__(self):
-        self.list = []
+        self.my_list = []
 
-    def add_task(self, task):
-        # task/s added to the list
-        self.list.append(task)
-        # nothing returned
+    def add_todo_task(self, list):
+        for task in list:
+            self.my_list.append(task)
 
-    def list_of_tasks(self):
-        # list of tasks retured 
-        return self.list
-        
+    def remove_task_done(self, list):
+        for task in list:
+            self.my_list.remove(task)
+    def remaining_list_of_tasks(self):
+        pass
+        return self.my_list
+
+# task_tracker = TaskTracker()
+# task_tracker.add_todo_task(["Finish Phase2","Buy groceries","Buy Table","Buy Chair"])
+# task_tracker.remove_task_done(["Finish Phase2","Buy Table","Buy Chair"])
+# print(task_tracker.remaining_list_of_tasks())
--- test/test_TaskTracker.py
+++ test/test_TaskTracker.py
@@ -2,11 +2,9 @@ from lib.TaskTracker import *
 
 def test_task_tracker():
     task_tracker = TaskTracker()
-    task_tracker.add_task('shopping')
-    task_tracker.add_task('walk')
-    task_tracker.add_task('call my friend')
-
-    result = task_tracker.list_of_tasks()
-
-    assert result == ['shopping', 'walk', 'call my friend']
-    
+    task_tracker.add_todo_task(['shopping','walk','call my friend'])
+    task_tracker.remaining_list_of_tasks()
+    task_tracker.remove_task_done(['shopping'])
+    result = task_tracker.remaining_list_of_tasks()
+    assert result == ['walk','call my friend']
+   
